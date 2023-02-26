b4ce7401b4a890cee48ef26253c9f20d804db870
Ayush Agarwal <ayushba2@illinois.edu>
2023-02-26 02:47:24 -0600

    user route added

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,11 @@ const express = require('express');
 const router = express.Router();
 const {connectToDB} = require('./utils');
 
-router.get('/get_userinfo?user_id=:user_id', (req, res) => {
+router.get('/get_userinfo?user_id=:user_id', async (req, res) => {
     let user_id = req.params.user_id;
     let sql = `SELECT firstname, lastname, email FROM Users WHERE user_id = ${user_id};`;
-    let response = connectToDB(sql);
+    let response = await connectToDB(sql);
     res.send(response);
 });
+
+module.exports = router;
