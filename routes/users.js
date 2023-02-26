92b0cbc1f88532fda70bccbb175cd15cd6ffe6ad
Prithvi Dharmaraj <prithvidraj@gmail.com>
2023-02-26 02:45:33 -0600

    fixed route syntax for user

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 const {connectToDB} = require('./utils');
 
-router.get('/get_userinfo?user_id=user_id', (req, res) => {
+router.get('/get_userinfo?user_id=:user_id', (req, res) => {
     let user_id = req.params.user_id;
     let sql = `SELECT firstname, lastname, email FROM Users WHERE user_id = ${user_id};`;
     let response = connectToDB(sql);
