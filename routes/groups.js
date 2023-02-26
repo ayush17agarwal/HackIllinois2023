f05ef0117b3ac3b00a6b75a241b09d52c50775f1
Ayush Agarwal <ayushba2@illinois.edu>
2023-02-26 02:49:02 -0600

    removed group/join

--- routes/groups.js
+++ routes/groups.js
@@ -12,21 +12,4 @@ router.get('/my_groups', (req, res) => {
     res.send(response);
 });
 
-//create group in splitwise.js
-
-router.post('/join', (req, res) => {
-    let group_id = req.body.group_id;
-    let user_id = req.body.user_id;
-
-    let sql = `INSERT INTO Group_users (group_id, user_id) VALUES (${group_id}, ${user_id});`;
-
-    let response = utils.connectToDB(sql);
-
-    res.send(response);
-});
-
-router.get('/', (req, res) => {
-    res.send("test groups");
-});
-
 module.exports = router;
