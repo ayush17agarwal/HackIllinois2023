74f527e27d99c6f6380d4118e0ef44e27c2e82f5
Ayush Agarwal <ayushba2@illinois.edu>
2023-02-26 03:11:15 -0600

    working users and groups

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,8 @@ const express = require('express');
 const router = express.Router();
 const {connectToDB} = require('./utils');
 
-router.get('/get_userinfo?user_id=:user_id', async (req, res) => {
-    let user_id = req.params.user_id;
+router.get('/get_userinfo', async (req, res) => {
+    let user_id = req.query.user_id;
     let sql = `SELECT firstname, lastname, email FROM Users WHERE user_id = ${user_id};`;
     let response = await connectToDB(sql);
     res.send(response);
