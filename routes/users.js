c6262f0add656f826a0f51b51d259af6e13de837
Prithvi Dharmaraj <prithvidraj@gmail.com>
2023-02-26 02:43:36 -0600

    added user route

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const mysql = require('mysql');
+
+const express = require('express');
+const router = express.Router();
+const {connectToDB} = require('./utils');
+
+router.get('/get_userinfo?user_id=user_id', (req, res) => {
+    let user_id = req.params.user_id;
+    let sql = `SELECT firstname, lastname, email FROM Users WHERE user_id = ${user_id};`;
+    let response = connectToDB(sql);
+    res.send(response);
+});
